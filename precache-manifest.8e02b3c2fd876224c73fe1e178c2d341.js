36a94859a68d0f150e002dfb1a1bc2acb16f3fd0
ItzJammyZz <jammy-anwar@hotmail.co.uk>
2020-09-14 00:45:58 +0100

    Updates

---
+++ precache-manifest.8e02b3c2fd876224c73fe1e178c2d341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1883621520eedac4bcfdb81f7ea9778d",
+    "url": "/Amazon-Clone/index.html"
+  },
+  {
+    "revision": "a9da87b74097d2f3a5e2",
+    "url": "/Amazon-Clone/static/css/main.12201203.chunk.css"
+  },
+  {
+    "revision": "d276d4dcc572c2731eec",
+    "url": "/Amazon-Clone/static/js/2.33b40f94.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/Amazon-Clone/static/js/2.33b40f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9da87b74097d2f3a5e2",
+    "url": "/Amazon-Clone/static/js/main.482dfbc9.chunk.js"
+  },
+  {
+    "revision": "503809e725826a7e4bc7",
+    "url": "/Amazon-Clone/static/js/runtime-main.c6ec4804.js"
+  }
+]);
